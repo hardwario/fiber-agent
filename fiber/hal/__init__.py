198cc45db84db078bb05250fecc2eec3d10feb19
nikitach507 <chuprinnikita8@gmail.com>
2024-04-05 17:33:02 +0200

    Removing of i2c implementation and code refactoring

--- fiber/hal/__init__.py
+++ fiber/hal/__init__.py
@@ -1,19 +1,11 @@
-import os
 import threading
-import time
 from fiber.common.queue_manager import QueueManager
 from fiber.common.thread_manager import pool
 from loguru import logger
-from fiber.hal.consts import GPIO_POWER_LED
-from fiber.hal.devices.probe_manager import ProbeLEDs, ProbePower
-from fiber.hal.devices.eeprom import EEPROM
-from fiber.common.gpio_manager import gpio_manager
+from fiber.hal.spidisplay import SPIDisplay
 from fiber.server.handlers import ServerHandler, NotFoundError
-from fiber.hal.i2c import I2C
-from fiber.hal.led_controller import Controller, LedControllerError
+from fiber.hal.led_controller import Controller
 from fiber.server.manager import (ServerManager, ServerError, ServerStopEventError)
-from fiber.server.serial_number import SerialNumberReadError
-from gpiod.line import Value
 
 class FiberHALError(Exception):
     def __init__(self, message: str):
@@ -28,22 +20,23 @@ class FiberHAL:
 
         self.stop_event = threading.Event()
         try:
-            self.initialize_i2c_devices()
-
-            self.led_controller = Controller(self.probe_leds)
+            self.led_controller = Controller()
+            self.spi_display = SPIDisplay()
             self.server = ServerManager(self.server_response_queue, self.client_request_queue, self.message_for_server_queue)
-            self.handler = ServerHandler(self.eeprom, self.server, self.led_controller, self.probe_leds, self.interface)
+            self.handler = ServerHandler(self.server, self.led_controller, self.interface)
+            self.led_controller.activate_leds()
+            self.spi_display.run_in_thread()
         except (ServerError, SystemError) as e:
             self.die(f"Problem attaching Server: {e}")
 
     def start(self) -> None:
-        logger.debug("Starting system thread...")
-        system_thread = threading.Thread(target=self.system_main_loop)
+        logger.debug("Starting hal thread...")
+        system_thread = threading.Thread(target=self.hal_main_loop)
         pool.manage_thread(True, system_thread, self.stop_event)
         system_thread.start()
 
-    def system_main_loop(self) -> None:
-        logger.info("System: OK")
+    def hal_main_loop(self) -> None:
+        logger.info("HAL: OK")
         while not self.stop_event.is_set():
             try:
                 uuid, request, body = self.server.recv(self.stop_event)
@@ -56,33 +49,8 @@ class FiberHAL:
             except NotFoundError:
                 self.server.send_err(request, uuid)
                 self.die(f"Request: Command not found for UUID: {uuid}, Request: {request}")  
-
-    def initialize_i2c_devices(self):
-        try:
-            self.local_i2c = I2C(10)
-            self.eeprom = EEPROM(self.local_i2c, device_addr=0x56) 
-            self.probe_power = ProbePower(self.local_i2c)
-            self.probe_leds = ProbeLEDs(self.local_i2c) 
-        except (OSError, TypeError) as e:
-            self.die(f"i2C initialization failed: {e}") 
-        except SerialNumberReadError as e:
-            self.critical_state(f"Problem initializing system: {e}")
-            self.die(f"Problem attaching EEPROM and SerialNumber: {e}") 
     
     def die(self, error_msg: str) -> None:
         logger.error(error_msg)
-        gpio_manager.release()
         raise SystemError(error_msg)
-
-    def critical_state(self, error_msg: str) -> None:
-        try:
-            logger.critical(error_msg)
-            while True:
-                gpio_manager.set_value(GPIO_POWER_LED, Value.ACTIVE)
-                time.sleep(0.05)
-                gpio_manager.set_value(GPIO_POWER_LED, Value.INACTIVE)
-                time.sleep(0.05)
-        except KeyboardInterrupt:
-            logger.info("KeyboardInterrupt received, stopping...")
-            gpio_manager.set_value(GPIO_POWER_LED, Value.INACTIVE)
 
