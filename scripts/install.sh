5eb9a2237a9f7c9718d2c0959d04d28372b354b2
nikitach507 <chuprinnikita8@gmail.com>
2024-04-05 17:02:30 +0200

    Update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+pip uninstall -y fiber
 
-echo "Running Ansible playbook to install..."
+cd /app
 
-# Путь до вашего файла hosts и install.yml может отличаться
-ansible-playbook -i /home/fiber/install/hosts /home/fiber/install/install.yml --ask-pass -K
+cp /app/install/roles/system/files/rc.local /etc/rc.local
 
-echo "Installation complete."
+cp /app/install/roles/system/files/journald.conf /etc/systemd/journald.conf
+
+cp /app/install/roles/fiber-packages/files/fiber-core.service /etc/systemd/system/fiber-core.service
+
+python /app/fiber/setup.py install
+
+mkdir /etc/fiber
+
+mkdir /var/fiber
+
+cp /app/config/config.yaml /etc/fiber/config.yaml
+
+systemctl enable fiber-core.service
+
+systemctl start fiber-core.service
+
+systemctl restart fiber-core.service
+
+# tmpfs /var/log  tmpfs defaults,noatime,nosuid,mode=0755,size=512m 0 0
+
+# echo "Running Ansible playbook to install..."
+
+# ansible-playbook -i /app/install/hosts /app/install/install.yml --ask-pass -vvv
+
+# echo "Installation complete."
+
