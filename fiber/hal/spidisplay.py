87f53f0472b6d97556149bc6e221724d590c9ed2
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 10:10:57 +0000

    Add an implementation to set values in the display

--- fiber/hal/spidisplay.py
+++ fiber/hal/spidisplay.py
@@ -13,10 +13,10 @@ class SPIDisplay:
         self._lock = Lock()
 
         self.display = ST7920Display()
-        sensor_widget = FiberSensorWidget(width=self.display.get_width())
+        self._sensor_widget = FiberSensorWidget(width=self.display.get_width())
 
         self.display.add_widget(DateTimeBanner(128), 0, 0, 0)
-        self.display.add_widget(sensor_widget, 0, 16, 0)
+        self.display.add_widget(self._sensor_widget, 0, 16, 0)
 
     def run_in_thread(self) -> None:
         if self.display_thread is not None:
@@ -30,3 +30,11 @@ class SPIDisplay:
     def display_main_loop(self) -> None:
         while not self._stop_event.wait(1):
             self.display.run()
+
+    def set_value(self, channel: int, value: float | None) -> None:
+        with self._lock:
+            self._sensor_widget.set_value(channel, value)
+
+    def set_voltage(self, eth_power: float, bat_power: float) -> None:
+        with self._lock:
+            self._sensor_widget.set_voltage(eth_power / 100, bat_power / 100)
