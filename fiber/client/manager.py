85425502b61027b9c830b7f7bdb2de029693a2bd
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 09:29:04 +0000

    Expand data type options for 'body' in ServerResponse model

--- fiber/client/manager.py
+++ fiber/client/manager.py
@@ -11,7 +11,7 @@ class ServerResponse(BaseModel):
     uuid: str = None  
     response: str = None
     error: bool
-    body: dict | int = None
+    body: dict | int | float | str = None
 
 
 class ClientManager:
@@ -39,8 +39,8 @@ class ClientManager:
 
     def _recv(self) -> int | float | str:
         try:
+            logger.debug("Client RECV: Trying to get message from server")
             recv = self.server_response_queue.recv_qmsg(self._stop_event)
-
             if recv is not None:
                 self.check_response(recv)
                 return recv["body"]
@@ -51,11 +51,9 @@ class ClientManager:
 
     def get_response(self, request_type: str) -> int | float | str:
         request_data = {"uuid": str(uuid1()), "request": request_type}
-
-        logger.debug(f"Cli: Sending request: \n{request_data}")
+        logger.debug(f"Client GET: Sending request: \n{request_data}")
         self.client_request_queue.send_qmsg(request_data)
         response = self._recv()
-        logger.debug(f"Cli: Recieve from {request_type} = {response}")
         return response
 
     def send_request(self, request_type: str, body: dict) -> None:
@@ -64,5 +62,5 @@ class ClientManager:
             "request": request_type,
             "body": body,
         }
-        logger.debug(f"Cli: Sending request: {request_data}")
+        logger.debug(f"Client SEND: Sending request: {request_data}")
         self.message_for_server_queue.send_qmsg(request_data)
