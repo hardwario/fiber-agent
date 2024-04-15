b8588b4a1f0447735bfdfea929850baf82dad710
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 10:10:01 +0000

    Enhance temperature processing and update indicator logic in Sensor

--- fiber/sensor/sensor.py
+++ fiber/sensor/sensor.py
@@ -3,7 +3,6 @@ import os
 import re
 import threading
 import time
-from fiber.common.consts import INDICATOR_GREEN, INDICATOR_RED
 from fiber.common.queue_manager import QueueManager
 from fiber.client.handler import ClientHandler
 from fiber.common.thread_manager import pool
@@ -15,7 +14,7 @@ class SensorError(Exception):
         super().__init__(message)
 
 class Sensor:
-    def __init__(self, channel: int, bus_directory: str, bulk_read: bool, sensor_temperature_queue: QueueManager, server_response_queue: QueueManager, client_request_queue: QueueManager, message_for_server_queue: QueueManager) -> None:
+    def __init__(self, channel: int, bus_directory: str, bulk_read: bool, sensor_lock, sensor_temperature_queue: QueueManager, server_response_queue: QueueManager, client_request_queue: QueueManager, message_for_server_queue: QueueManager) -> None:
         self.channel = channel
         self.bus_directory = bus_directory
         self.bulk_read = bulk_read
@@ -24,6 +23,7 @@ class Sensor:
         self.client_request_queue = client_request_queue
         self.message_for_server_queue = message_for_server_queue
         self.stop_event = threading.Event()
+        self.sensor_lock = sensor_lock
         self.known = {}
         try:
             self.therm_bulk_read = os.path.join(bus_directory, "therm_bulk_read")
@@ -61,45 +61,48 @@ class Sensor:
             self.update_indicator()
             time.sleep(1)
 
-    def process_thermometer(self, thermometer: str) -> None:
+    def process_thermometer(self, thermometer: str) -> tuple[float | None, bool]:
         thermometer_path = os.path.join(self.bus_directory, thermometer)
         if not os.path.isdir(thermometer_path) or re.match(r"^28-[0-9a-f]{12}$", thermometer) is None:
             return True
 
-        ts = int(time.time())
-        self.known[thermometer] = ts
-
         temperature_path = os.path.join(thermometer_path, "temperature")
+
         try:
-            with open(temperature_path, "r") as f:
-                temperature = round(int(f.readline().strip()) / 1000, 2)
+            with self.sensor_lock:
+                with open(temperature_path, "r") as f:
+                    temperature = round(int(f.readline().strip()) / 1000, 2)
+                time.sleep(0.01)
+
+            self.known[thermometer] = temperature
 
             data = {
-                "timestamp": ts,
+                "timestamp": int(time.time()),
                 "channel": self.channel,
                 "thermometer": thermometer,
                 "temperature": temperature,
             }
-            logger.info(f'CHANNEL {self.channel} to queue - Temperature: ' f"{temperature} C")
-
+            if temperature is not None:
+                logger.info(f'CHANNEL {self.channel} to queue - Temperature: ' f"{temperature} C")
 
             self.sensor_temperature_queue.send_qmsg(data)
         except (OSError, ValueError):
-            self.known.pop(thermometer)
+            self.known.pop(thermometer) if self.known else ...
             return False
         return True
 
     def update_indicator(self) -> None:
         if len(self.known) == 0:
-            self.client.set_indicator(self.channel, INDICATOR_RED)
+            self.client.set_probe_indicator(self.channel, None)
         else:
-            self.client.set_indicator(self.channel, INDICATOR_GREEN)
+            temperature = next(iter(self.known.values()))
+            self.client.set_probe_indicator(self.channel, temperature)
 
     def trigger_bulk_read(self) -> None:
         try:
             if os.path.exists(self.therm_bulk_read):
                 with threading.Lock():
-                    with open(self.therm_bulk_read, "w") as f:
+                    with open(self.therm_bulk_read, "r+") as f:
                         f.write("trigger\n")
         except OSError as e:
             raise SensorError(f"Error triggering bulk read: {e}")
