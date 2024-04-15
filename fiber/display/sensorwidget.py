ee703a183d1108885d72d58352bed23872349227
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 09:47:06 +0000

    Add implementation for display power

--- fiber/display/sensorwidget.py
+++ fiber/display/sensorwidget.py
@@ -10,7 +10,7 @@ from fiber.display.src.widget import Widget
 @dataclass
 class FiberSensor:
     channel: int
-    value: float | None
+    value: float
 
 
 class FiberSensorWidget(Widget):
@@ -19,12 +19,16 @@ class FiberSensorWidget(Widget):
 
     _sensor_page: int
     _sensors: list[FiberSensor]
+    _eth_power: float
+    _bat_power: float
 
     def __init__(self, width: int, page_swap_time: int = 5):
-        super().__init__(width, 10 * 4)  # 9 pixels per sensor, 4 sensors
+        super().__init__(width, 10 * 5)  # 9 pixels per sensor, 4 sensors + 1 for voltage
         self._sensor_page = 0
         self._sensors = []
         self._timer = datetime.now() + timedelta(seconds=page_swap_time)
+        self._eth_power = None
+        self._bat_power = None
 
     def set_value(self, channel: int, value: float | None):
         # sets value for sensor, if it does not exist, add it
@@ -37,6 +41,11 @@ class FiberSensorWidget(Widget):
 
         self._sensors.append(FiberSensor(channel, value))
         self._sensors.sort(key=lambda x: x.channel)
+
+    def set_voltage(self, eth_power: float, bat_power: float):
+        self._eth_power = eth_power
+        self._bat_power = bat_power
+        self._changed = True
 
     def update(self):
         # every 5 seconds, swap to the next sensors
@@ -67,7 +76,11 @@ class FiberSensorWidget(Widget):
                 fill=255,
             )
 
-            sensor_text = f"{sensor.value} ºC" if sensor.value is not None else "---"
+            if sensor.value is not None:
+                sensor_text = f"{sensor.value:.2f} ºC"
+            else:
+                sensor_text = "--- ºC"
+
             draw.text(
                 (self.get_width() - draw.textlength(sensor_text), i * 9),
                 sensor_text,
@@ -75,3 +88,22 @@ class FiberSensorWidget(Widget):
                 align="right",
                 fill=255,
             )
+        
+        draw.text(
+            (0, 36),
+            f"PoE/Bat:",
+            font=FONT_SMALL,
+            fill=255,
+        )
+
+        if self._eth_power is not None and self._bat_power is not None:
+            power_text = f"{self._eth_power:.2f}/{self._bat_power:.2f}mV"
+        else:
+            power_text = "---/--- mV"
+        draw.text(
+            (self.get_width() - draw.textlength(power_text), 36),
+            power_text,
+            font=FONT_SMALL,
+            align="right",
+            fill=255,
+        )
