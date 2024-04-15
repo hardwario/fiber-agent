98542a03634da899c82eafef8570345b8157da94
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 09:42:15 +0000

    Add fiber path variable and probe indexes

--- fiber/common/consts.py
+++ fiber/common/consts.py
@@ -1,8 +1,4 @@
-INDICATOR_GREEN = "green"
-INDICATOR_RED = "red"
-
-INDICATOR_ON_TAG = "on"
-INDICATOR_OFF_TAG = "off"
+PATH_FIBER_FILE = "/var/fiber/"
 
 POWER_LED = 0
 PROBE_1 = 1
@@ -13,3 +9,15 @@ PROBE_5 = 5
 PROBE_6 = 6
 PROBE_7 = 7
 PROBE_8 = 8
+
+PROBE_INDEX = {
+    POWER_LED: [0, 1],
+    PROBE_1: [2, 3],
+    PROBE_2: [4, 5],
+    PROBE_3: [6, 7],
+    PROBE_4: [8, 9],
+    PROBE_5: [10, 11],
+    PROBE_6: [12, 13],
+    PROBE_7: [14, 15],
+    PROBE_8: [16, 17],
+}
