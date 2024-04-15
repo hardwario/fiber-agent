bd64b5933f16c6c637a586a826a8ad6db7eb1fa6
nikitach507 <chuprinnikita8@gmail.com>
2024-04-15 09:40:30 +0000

    Delete the method to check the indicator value

--- fiber/client/validator.py
+++ fiber/client/validator.py
@@ -1,7 +1,5 @@
-from fiber.common.consts import (INDICATOR_GREEN, INDICATOR_RED, 
-                                 INDICATOR_OFF_TAG, INDICATOR_ON_TAG,
-                                 POWER_LED, PROBE_1, PROBE_2, PROBE_3,
-                                 PROBE_4, PROBE_5, PROBE_6, PROBE_7, PROBE_8)
+from fiber.common.consts import (POWER_LED, PROBE_1, PROBE_2, PROBE_3,
+                                PROBE_4, PROBE_5, PROBE_6, PROBE_7, PROBE_8)
 
 
 class ClientDataValidator:
@@ -19,18 +17,4 @@ class ClientDataValidator:
             PROBE_8,
         }
         if probe not in valid_probes:
-            raise SystemError
-
-    @staticmethod
-    def validate_indicator(indicator: str, probe: int) -> None:
-        if probe == POWER_LED:
-            valid_indicators = {INDICATOR_ON_TAG, INDICATOR_OFF_TAG}
-        else:
-            valid_indicators = {
-                INDICATOR_RED,
-                INDICATOR_GREEN,
-                INDICATOR_ON_TAG,
-                INDICATOR_OFF_TAG,
-            }
-        if indicator not in valid_indicators:
             raise SystemError
