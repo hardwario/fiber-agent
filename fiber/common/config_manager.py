a3d8c2aa73a7eb6e37e02cea07093384c7328a73
nikitach507 <chuprinnikita8@gmail.com>
2024-04-29 14:27:09 +0000

    Change class implementation for flexibility in use

--- fiber/common/config_manager.py
+++ fiber/common/config_manager.py
@@ -3,46 +3,27 @@ from pydantic import BaseModel
 from loguru import logger
 
 
-class SystemConfig(BaseModel):
-    interface: str
-
-class SensorConfig(BaseModel):
-    enabled: bool
-    report_interval_seconds: int
-    sampling_interval_seconds: int
-
-class MQTTConfig(BaseModel):
-    enabled: bool
-    host: str
-    port: int
-
-class StorageConfig(BaseModel):
-    enabled: bool
-    name: str
-
-class FiberConfig(BaseModel):
-    version: int
-    system: SystemConfig
-    sensor: SensorConfig
-    mqtt: MQTTConfig
-    storage: StorageConfig
-
-
 class ConfigManagerError(Exception):
     pass
 
 class ConfigManager:
-    def __init__(self, config_path: str) -> None:
+    def __init__(self, config_path: str, config_model: BaseModel) -> None:
         self.config_path = config_path
-        self.config_data = self.load_config()
+        self.config_model = config_model
+        self.config_data = self.load_config_from_file()
 
-    def load_config(self) -> FiberConfig:
+    def load_config_from_file(self) -> BaseModel:
         try:
             with open(self.config_path, 'r') as yaml_file:
                 parsed_yaml = yaml.safe_load(yaml_file)
-            return FiberConfig(**parsed_yaml)
-        except (FileNotFoundError, yaml.YAMLError) as e:
-            raise Exception(f"Configuration load error: {e}")
+                logger.info(f"Configuration loaded: {parsed_yaml} with type {type(parsed_yaml)}")
+            return self.config_model(**parsed_yaml)
+        except FileNotFoundError as e:
+            logger.error(f"File not found: {e}")
+            raise ConfigManagerError(f"Configuration file not found: {e}")
+        except yaml.YAMLError as e:
+            logger.error(f"YAML load error: {e}")
+            raise ConfigManagerError(f"Configuration load error: {e}")
 
     def save_config(self) -> None:
         try:
@@ -50,24 +31,5 @@ class ConfigManager:
                 yaml.safe_dump(self.config_data.dict(), yaml_file, default_flow_style=False)
             logger.info("Configuration updated")
         except yaml.YAMLError as e:
+            logger.error(f"YAML save error: {e}")
             raise ConfigManagerError(f"Configuration save error: {e}")
-        
-    def set_interface(self, value: str) -> None:
-        self.config_data.system.interface = value
-        self.save_config()
-
-    def set_report_interval_seconds(self, value: int) -> None:
-        self.config_data.sensor.report_interval_seconds = value
-        self.save_config()
-
-    def set_sampling_interval_seconds(self, value: int) -> None:
-        self.config_data.sensor.sampling_interval_seconds = value
-        self.save_config()
-
-    def set_mqtt_hostname(self, value: str) -> None:
-        self.config_data.mqtt.host = value
-        self.save_config()
-
-    def set_mqtt_port(self, value: int) -> None:
-        self.config_data.mqtt.port = value
-        self.save_config()
