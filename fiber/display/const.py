f382f8f505014bb72ec07d2bbd446803f1e54c41
nikitach507 <chuprinnikita8@gmail.com>
2024-04-29 14:32:42 +0000

    Update GPIO and PWM constants

--- fiber/display/const.py
+++ fiber/display/const.py
@@ -2,7 +2,8 @@ from PIL import ImageFont
 
 
 RESET_GPIO = 16
-CHIP_SELECT_GPIO = 22
 BUZZER_GPIO = 17
+BRIGHTNESS_PWM_GPIO = 13
+PWM_HALF_PERIOD = 8  
 
 FONT_SMALL = ImageFont.truetype("/app/fiber/display/LiberationSans-BaDn.ttf", size=9)
