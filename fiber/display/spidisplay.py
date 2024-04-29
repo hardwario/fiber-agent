591aa26710b4eaac6978a7c2739639afb9973c19
nikitach507 <chuprinnikita8@gmail.com>
2024-04-29 14:34:10 +0000

    Add class for SPI-based display functionality

---
+++ fiber/display/spidisplay.py
@@ -0,0 +1,51 @@
+
+from loguru import logger
+from threading import Thread, Event, Lock
+from fiber.display.examplewidgets import DateTimeBanner  
+from fiber.display.sensorwidget import FiberSensorWidget
+from fiber.display.st7920display import ST7920Display
+
+
+class SPIDisplay:
+    def __init__(self):
+        self._display_thread = Thread(target=self._loop)
+        self._stop_event = Event()
+        
+        self._lock = Lock()
+
+        self.display = ST7920Display(width=128, height=64, brightness=100)
+        self._sensor_widget = FiberSensorWidget(width=self.display.get_width())
+
+        self.display.add_widget(DateTimeBanner(128), 0, 0, 0)
+        self.display.add_widget(self._sensor_widget, 0, 16, 0)
+    
+    def close(self) -> None:
+        with self._lock:
+            self._stop_event.set()
+
+            if self._display_thread is not None:
+                self._display_thread.join()
+            
+                if self._display_thread.is_alive():
+                    logger.error("Thread did not exit in time")
+                else:
+                    logger.info(f"Thread {self._display_thread.name} exited")
+    
+    def start(self) -> None:
+        logger.info("SPI Display: OK")
+        self._display_thread.start()
+
+    def _loop(self) -> None:
+        while not self._stop_event.wait(0.2):
+            self.display.run()
+
+        self.display.close()
+
+
+    def set_value(self, channel: int, value: float | None) -> None:
+        with self._lock:
+            self._sensor_widget.set_value(channel, value)
+
+    def set_voltage(self, eth_power: float, bat_power: float) -> None:
+        with self._lock:
+            self._sensor_widget.set_voltage(eth_power / 100, bat_power / 100)
