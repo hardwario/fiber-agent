1bd40ce9fce740c92c142c5c0695e9a92d5ef89f
nikitach507 <chuprinnikita8@gmail.com>
2024-04-29 14:41:23 +0000

    Modify the structures and implementations of the MQTT class

--- fiber/mqtt/mqtt_bridge.py
+++ fiber/mqtt/mqtt_bridge.py
@@ -1,12 +1,13 @@
 import json
 import threading
-
+import schedule
+import time
 import paho.mqtt.client as mqtt
-from pydantic import ValidationError
 from fiber.client.handler import ClientHandler
 from loguru import logger
-
-from fiber.common.config_manager import ConfigManager, ConfigManagerError
+from fiber.common.config_manager import ConfigManagerError
+from fiber.models.configurations import FiberConfig
+from fiber.models.system import BeaconBody
 
 
 class MQTTError(Exception):
@@ -18,39 +19,48 @@ class CallbackError(Exception):
 
 
 class MQTTBridge:
-    def __init__(self, client_handler: ClientHandler, config_path: str) -> None:
-        self._topic_callback = {}
+    def __init__(self, core_stop_event: threading.Event, client_handler: ClientHandler, fiber_config: FiberConfig) -> None:
+        self._topic_callback: dict[str, callable] = {}
         self.client_handler = client_handler
-        self.mqtt_client = mqtt.Client()
-        self._fiber_config = ConfigManager(config_path).config_data
         self._lock = threading.RLock()
-
+        self._core_stop_event = core_stop_event
         self._fiber_id = self.client_handler.get_fiber_id()
-        self.mqtt_client.on_connect = self._on_connect
-        self.mqtt_client.on_message = self._on_message
-        self.mqtt_client.on_disconnect = self._on_disconnect
+        self._fiber_config = fiber_config
 
-        self.mqtt_client.user_data_set(
-            {"bridge": self, "fiber_id": self._fiber_id}
-        )
+        self.mqtt_client = self._create_mqtt_client()
+
+    def close(self) -> None:
+        self.mqtt_client.loop_stop()
+
+        self._core_stop_event.set()
+
+        if self.mqtt_thread is not None:
+            self.mqtt_thread.join()
+            if self.mqtt_thread.is_alive():
+                logger.error("Thread did not exit in time")
+            else:
+                logger.info(f"Thread {self.mqtt_thread.name} exited")
+
+    def _create_mqtt_client(self) -> mqtt.Client | None:
+        if self._fiber_config.mqtt.host is None or self._fiber_config.mqtt.port is None:
+            return None
+
+        mqtt_client = mqtt.Client()
+        mqtt_client.on_connect = self._on_connect
+        mqtt_client.on_message = self._on_message
+        mqtt_client.on_disconnect = self._on_disconnect
+
+        mqtt_client.user_data_set({"bridge": self, "fiber_id": self._fiber_id})
 
         try:
-            if self._fiber_config.mqtt.host is None or \
-                self._fiber_config.mqtt.port is None:
-                self.mqtt_client = None
-
-            else:
-                self.mqtt_client.connect(host=self._fiber_config.mqtt.host,
-                                     port=int(self._fiber_config.mqtt.port))
-
-                self.mqtt_client.loop_start()
+            mqtt_client.connect(host=self._fiber_config.mqtt.host, port=int(self._fiber_config.mqtt.port))
+            mqtt_client.loop_start()
         except ConnectionError as exc:
             raise MQTTError from exc
 
-    def __del__(self) -> None:
-        self.mqtt_client.loop_stop()
+        return mqtt_client
 
-    def _acc(self, topic: str, callback):
+    def _acc(self, topic: str, callback: callable) -> None:
         topic = f"fiber/{self._fiber_id}{topic}"
         self._topic_callback[topic] = callback
 
@@ -60,28 +70,34 @@ class MQTTBridge:
 
     def _on_connect(self, client: mqtt.Client, userdata, flags, rc) -> None:
         client.subscribe(f"fiber/{self._fiber_id}/#", 0)
-
-        self._acc("/system/reboot", self._callback_reboot)
-        self._acc("/config/get", self._callback_config_get)
-        self._acc("/config/set", self._callback_config_set)
-        self._acc("/system/mac/get", self._callback_mac_get)
-        self._acc("/system/ip/get", self._callback_ip_get)
-        self._acc("/system/uptime/get", self._callback_uptime_get)
-
-
         logger.debug("MQTT: Subscribed communication topics")
 
     def _on_message(self, client, userdata, msg: mqtt.MQTTMessage) -> None:
+        logger.info(f"Received message on topic {msg.topic} with payload {msg.payload}")
+
         try:
             if msg.topic in self._topic_callback:
-                logger.debug(f"Topic of the msg: {msg.topic}: {self._topic_callback[msg.topic]}")
-                self._topic_callback[msg.topic](
-                    msg.topic, json.loads(msg.payload)
-                )
+                callback = self._topic_callback[msg.topic]
+                callback(msg.payload)
             else:
                 logger.debug(f"No callback found for topic: {msg.topic}")
-        except (CallbackError, json.JSONDecodeError, KeyError):
+        except (CallbackError, json.JSONDecodeError, KeyError, ConfigManagerError):
             self.send_error(msg.topic)
+
+    def start(self) -> None:
+        self.mqtt_thread = threading.Thread(target=self._loop)
+        self.mqtt_thread.start()
+
+    def _loop(self) -> None:
+        schedule.every(1).minute.do(self._send_beacon_data).run()
+
+        logger.info("MQTT: OK")
+        while not self._core_stop_event.is_set():
+            try:
+                schedule.run_pending()
+                time.sleep(0.1)
+            except MQTTError as e:
+                raise SystemError(e)
 
     def send_ok(self, topic: str) -> None:
         logger.debug(f"Sending OK to {topic}/result")
@@ -103,110 +119,28 @@ class MQTTBridge:
                 )
 
     def send_json(self, topic: str, obj: dict | None, prefix="fiber/") -> None:
-        logger.debug(
-            f"Sending JSON object message on {prefix}{self._fiber_id}{topic}"
-            f" with object {obj}"
-        )
-
+        logger.debug(f"Sending JSON object message on {prefix}{self._fiber_id}{topic} with object {obj}")
         if self.mqtt_client:
             try:
                 if obj is not None:
                     obj = json.dumps(obj)
 
                     with self._lock:
-                        self.mqtt_client.publish(
-                            f"{prefix}{self._fiber_id}{topic}", obj
-                        )
+                        self.mqtt_client.publish(f"{prefix}{self._fiber_id}{topic}", obj)
                 else:
                     logger.warning("No data to send as JSON")
             except json.JSONDecodeError:
                 self.send_error(topic)
         else:
-            logger.debug("Can't publish: no MQTT broker defined")   
-
-    def send_config(self) -> None:
-        logger.debug("Send configuration")
-        topic = "/config"
-        config = self._fiber_config.dict()
-        self.send_json(topic, config)
-
-    def send_ip(self) -> None:
-        logger.debug("Send IP address")
-        topic = "/system/ip"
-        ip = self.client_handler.get_ip()
-        self.send_json(topic, ip)
-
-    def send_mac(self) -> None:
-        logger.debug("Send MAC address")
-        topic = "/system/mac"
-        mac = self.client_handler.get_mac()
-        self.send_json(topic, mac)
-
-    def send_uptime(self) -> None:
-        logger.debug("Send uptime")
-        topic = "/system/uptime"
-        uptime = self.client_handler.get_uptime()
-        self.send_json(topic, uptime)
-
-    def send_measurements(self, data: dict) -> None:
-        logger.debug(f"Send measurements: {data}")
-        self.send_json("/measurement", data)
-
-    def send_beacon(self) -> None:
+            logger.debug("Can't publish: no MQTT broker defined") 
+            
+    def _send_beacon_data(self) -> None:
         try:
-            beacon_data = self._prepare_beacon_data()
-            self.send_json("/beacon", beacon_data)
+            uptime = self.client_handler.get_uptime()
+            ip_address = self.client_handler.get_ip()
+            mac = self.client_handler.get_mac()
+
+            beacon_data = BeaconBody(uptime=uptime, ip_address=ip_address, mac_address=mac)
+            self.send_json("/beacon", beacon_data.dict())
         except SystemError:
-            logger.debug("Problem while receiving uptime, ip_address or mac_address")
-
-    def _prepare_beacon_data(self) -> dict:
-        uptime = self.client_handler.get_uptime()
-        ip_address = self.client_handler.get_ip()
-        mac = self.client_handler.get_mac()
-        
-        logger.debug(f'SYSTEM INFO: ip - {ip_address}, mac - {mac}')
-        return {"uptime": uptime, "ip_address": ip_address, "mac_address": mac}
-
-    def _callback_reboot(self, topic, payload) -> None:
-        logger.debug("Reboot request")
-        self.client_handler.reboot()
-
-    def _callback_config_set(self, msg: mqtt.MQTTMessage) -> None:
-        logger.info("Set configuration")
-        try:
-            payload_json = json.loads(msg.payload)
-            try:
-                updated_config = self._fiber_config(**payload_json)
-                self._fiber_config = updated_config
-            except ValidationError as e:
-                logger.error(f"Payload validation error: {e}")
-        except (ConfigManagerError, json.JSONDecodeError):
-            raise ValueError
-        
-    def _callback_ip_get(self, topic: str, payload) -> None:
-        try:
-            self.send_ip()
-            self.send_ok(topic)
-        except SystemError:
-            self.send_error(topic)
-
-    def _callback_mac_get(self, topic: str, payload) -> None:
-        try:
-            self.send_mac()
-            self.send_ok(topic)
-        except SystemError:
-            self.send_error(topic)
-
-    def _callback_uptime_get(self, topic: str, payload) -> None:
-        try:
-            self.send_uptime()
-            self.send_ok(topic)
-        except SystemError:
-            self.send_error(topic)
-
-    def _callback_config_get(self, topic: str, payload) -> None:
-        try:        
-            self.send_config()
-            self.send_ok(topic)
-        except SystemError:
-            self.send_error(topic)
+            logger.debug("Problem while receiving uptime, ip_address or mac_address") 
---
+++ fiber/mqtt/mqtt_handler.py
@@ -0,0 +1,76 @@
+import paho.mqtt.client as mqtt
+import json
+
+from loguru import logger
+from pydantic import ValidationError
+from fiber.common.config_manager import ConfigManagerError
+from fiber.models.configurations import FiberConfig
+from fiber.mqtt.mqtt_bridge import MQTTBridge
+
+
+class MQTTHandler(MQTTBridge):
+    def _on_connect(self, client: mqtt.Client, userdata, flags, rc) -> None:
+        super()._on_connect(client, userdata, flags, rc)
+        self._acc("/config/get", self.send_config)
+        self._acc("/config/set", self.set_config)
+        self._acc("/system/mac/get", self.send_mac)
+        self._acc("/system/ip/get", self.send_ip)
+        self._acc("/system/uptime/get", self.send_uptime)
+        self._acc("/system/reboot", self.system_reboot)     
+
+    def send_ip(self, payload: None) -> None:
+        try:
+            topic = "/system/ip"
+            ip = self.client_handler.get_ip()
+            self.send_json(topic, ip)            
+            self.send_ok(topic)
+        except SystemError:
+            self.send_error(topic)
+
+    def send_mac(self, payload: None) -> None:
+        try:
+            my_topic = "/system/mac"
+            mac = self.client_handler.get_mac()
+            self.send_json(my_topic, mac)
+            self.send_ok(my_topic)
+        except SystemError:
+            self.send_error(my_topic)
+
+    def send_uptime(self, payload: None) -> None:
+        try:
+            topic = "/system/uptime"
+            uptime = self.client_handler.get_uptime()
+            self.send_json(topic, uptime)
+            self.send_ok(topic)
+        except SystemError:
+            self.send_error(topic)
+
+    def send_config(self, payload: None) -> None:
+        try:        
+            topic = "/config"
+            config = self._fiber_config.dict()
+            self.send_json(topic, config)
+            self.send_ok(topic)
+        except SystemError:
+            self.send_error(topic)
+
+    def set_config(self, payload: dict) -> None:
+        logger.info("Set configuration")
+        try:
+            topic = "/config"
+            payload_json = json.loads(payload)
+            updated_config = FiberConfig(**payload_json)
+            self._fiber_config = updated_config
+            self.send_ok(topic)
+        except (ValidationError, TypeError) as e:
+            logger.error(f"Payload validation error: {e}")
+        except (ConfigManagerError, json.JSONDecodeError):
+            self.send_error(topic) 
+
+    def system_reboot(self, payload: None) -> None:
+        logger.debug("Reboot request")
+        self.client_handler.reboot()
+
+    def send_measurements(self, data: dict) -> None:
+        topic = "/measurement"
+        self.send_json(topic, data)
