a1b4be3fffd7f9f3ccc4518ede5618b775bb234c
nikitach507 <chuprinnikita8@gmail.com>
2024-04-29 14:24:19 +0000

    Delete queue and add base models

--- fiber/client/manager.py
+++ fiber/client/manager.py
@@ -1,66 +1,52 @@
-from uuid import uuid1
+
+from uuid import uuid1, UUID
 import threading
-from pydantic import BaseModel, ValidationError
+from pydantic import ValidationError
 import json
 from loguru import logger
 
+from fiber.models.request_response import Request, Response
 from fiber.common.queue_manager import QueueManager
 
 
-class ServerResponse(BaseModel):
-    uuid: str = None  
-    response: str = None
-    error: bool
-    body: dict | int | float | str = None
-
-
 class ClientManager:
-    def __init__(
-        self,
-        server_response_queue: QueueManager,
-        client_request_queue: QueueManager,
-        message_for_server_queue: QueueManager,
-        stop_event: threading.Event,
-    ) -> None:
+    def __init__(self, core_stop_event: threading.Event, server_response_queue: QueueManager, client_request_queue: QueueManager) -> None:
         self.server_response_queue = server_response_queue
         self.client_request_queue = client_request_queue
-        self.message_for_server_queue = message_for_server_queue
-        self._stop_event = stop_event
+        self._core_stop_event = core_stop_event
 
-    def check_response(self, resp: dict) -> None:
+    def check_response(self, received_msg: dict) -> Response:
         try:
-            ServerResponse(**resp)
+            msg = Response(**received_msg)
         except ValidationError as e:
             raise SystemError(e)
 
-        if resp["error"] is True:
+        if msg.error is True:
             logger.error("Server response error")
             raise SystemError
+        
+        return msg
 
     def _recv(self) -> int | float | str:
         try:
             logger.debug("Client RECV: Trying to get message from server")
-            recv = self.server_response_queue.recv_qmsg(self._stop_event)
-            if recv is not None:
-                self.check_response(recv)
-                return recv["body"]
+            received_msg = self.server_response_queue.recv_qmsg(stop_event=self._core_stop_event)
+            if received_msg is not None:
+                msg = self.check_response(received_msg)
+                return msg.body
             return None
         except (json.JSONDecodeError, KeyError):
             logger.error("System recieve error")
             raise SystemError
 
-    def get_response(self, request_type: str) -> int | float | str:
-        request_data = {"uuid": str(uuid1()), "request": request_type}
-        logger.debug(f"Client GET: Sending request: \n{request_data}")
-        self.client_request_queue.send_qmsg(request_data)
+    def get_response(self, operation: str) -> int | float | str:
+        request_data = Request(uuid=str(uuid1()), request=operation)
+        logger.debug(f"Client GET: Sending request: \n{dict(request_data)}")
+        self.client_request_queue.send_qmsg(dict(request_data))
         response = self._recv()
         return response
 
-    def send_request(self, request_type: str, body: dict) -> None:
-        request_data = {
-            "uuid": str(uuid1()),
-            "request": request_type,
-            "body": body,
-        }
-        logger.debug(f"Client SEND: Sending request: {request_data}")
-        self.message_for_server_queue.send_qmsg(request_data)
+    def send_request(self, operation: str, payload: dict) -> None:
+        request_data = Request(uuid=str(uuid1()), request=operation, body=payload)
+        logger.debug(f"Client SEND: Sending request: {dict(request_data)}")
+        self.client_request_queue.send_qmsg(dict(request_data))
