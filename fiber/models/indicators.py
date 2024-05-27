f8746d25de4e51842e8f43ee3dbffe7af6e7cd2b
nikitach507 <nikita.chuprin@hardwario.com>
2024-05-27 13:39:29 +0000

    Remove SensorDisplayBody class

--- fiber/models/indicators.py
+++ fiber/models/indicators.py
@@ -30,30 +30,3 @@ class StateIndicatorBody(BaseModel):
         return value
 
 
-class SensorDisplayBody(BaseModel):
-    '''
-    Represents the body for modifying the sensor display
-
-    Attributes:
-        output (int): The probe number.
-        temperature (int | float | None): The temperature value to determine the indicator color.
-            If None, the indicator color is set to red.
-            If a int or float value is provided, the indicator color is set to green.
-    '''
-
-    output: int
-    '''The probe number.'''
-    temperature: int | float | None
-    '''The temperature value to determine the indicator color.'''
-
-    @validator('output')
-    def validate_output(cls, value):
-        '''
-        Validates the output field.
-
-        Raises:
-            ValueError: If the value is not a valid probe number.
-        '''
-        if value not in VALID_PROBES:
-            raise ValueError('Invalid probe')
-        return value
