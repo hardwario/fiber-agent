b36cd175bb74c39f208c4fa72078a2d2a79f22e1
nikitach507 <nikita.chuprin@hardwario.com>
2024-05-27 16:03:34 +0000

    Refactor of variable names and error handling

--- fiber/buttons/button.py
+++ fiber/buttons/button.py
@@ -14,7 +14,7 @@ class ButtonController:
         self._stop_event = threading.Event()
 
         self.chip = gpiod.Chip(PATH_CHIP)
-        self.last_push = {gpio: 0 for gpio in GPIO_LINES}
+        self.last_press = {gpio: 0 for gpio in GPIO_LINES}
 
         self.st7920_display = spi_display.display
         self.sensor_widget = spi_display.sensor_widget
@@ -34,10 +34,9 @@ class ButtonController:
             consumer='button_controller', config=config)
 
     def quit(self) -> None:
-        logger.info('Before stop')
         self._stop_event.set()
 
-        if self._button_thread.is_alive():
+        if self._button_thread is not None:
             self._button_thread.join(timeout=10)
 
             if self._button_thread.is_alive():
@@ -54,40 +53,46 @@ class ButtonController:
         try:
             while not self._stop_event.is_set():
                 time.sleep(0.2)
-                check_push = self.request.wait_edge_events(0)
-                if check_push:
-                    push_events = self.request.read_edge_events()
-                    logger.info(f'Push events: {len(push_events)}')
+
+                try:
+                    is_press = self.request.wait_edge_events(0)
+                except gpiod.LineRequestError as e:
+                    logger.error(f'Error waiting for edge events: {e}')
+                    break
+
+                if is_press:
+                    try:
+                        press_events = self.request.read_edge_events()
+                    except gpiod.LineRequestError as e:
+                        logger.error(f'Error reading edge events: {e}')
+                        break
 
                     current_time = time.time()
-                    for event in push_events:
-                        if self._stop_event.is_set():
-                            break
-
+                    for event in press_events:
                         line_offset = event.line_offset
 
-                        if current_time - self.last_push[line_offset] > 0.5:
-                            self.last_push[line_offset] = current_time
+                        if current_time - self.last_press[line_offset] > 0.5:
+                            self.last_press[line_offset] = current_time
 
                             if line_offset == 23 and self.current_brightness < self.max_brightness:
                                 self.current_brightness += 20
                                 self.st7920_display.set_brightness(
                                     self.current_brightness)
                             elif line_offset == 24:
-                                self.sensor_widget.freeze_page()
+                                self.sensor_widget.freeze_page(freeze_time=30)
                             elif line_offset == 25 and self.current_brightness > 0:
                                 self.current_brightness -= 20
                                 self.st7920_display.set_brightness(
                                     self.current_brightness)
 
-                            self.st7920_display.set_buzzer(False)
-                            time.sleep(0.02)
-                            self.st7920_display.set_buzzer(True)
-        except Exception as e:
+                        self.st7920_display.set_buzzer(False)
+                        time.sleep(0.02)
+                        self.st7920_display.set_buzzer(True)
+        except (OSError, gpiod.LineRequestError, gpiod.ChipError) as e:
             logger.error(f'Error in buttons loop: {e}')
         finally:
             try:
                 self.request.release()
                 self.chip.close()
-            except Exception as e:
-                logger.error(f'Error with releasing lines: {e}')
+            except (gpiod.ChipError, gpiod.LineRequestError) as error:
+                logger.error(f'Error with releasing lines: {error}')
