c29cc7780e9b39ebc15e46346bb3b14b3dd3d9fe
nikitach507 <nikita.chuprin@hardwario.com>
2024-05-30 12:31:42 +0000

    Update PWM settings

--- fiber/display/const.py
+++ fiber/display/const.py
@@ -3,7 +3,8 @@ from PIL import ImageFont
 
 RESET_GPIO = 16
 BUZZER_GPIO = 17
-BRIGHTNESS_PWM_GPIO = 13
-PWM_HALF_PERIOD = 8  
+PWM_PERIOD = 1000000
+START_DUTY_CYCLE = 30
+
 
 FONT_SMALL = ImageFont.truetype('/app/fiber/display/LiberationSans-BaDn.ttf', size=9)
