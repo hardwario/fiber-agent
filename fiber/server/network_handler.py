5349ebce3f848718d019a361138499da8b383140
nikitach507 <nikita.chuprin@hardwario.com>
2024-06-05 12:46:54 +0000

    Refactor code

--- fiber/server/network_handler.py
+++ fiber/server/network_handler.py
@@ -9,22 +9,6 @@ class NetworkInterfaceHandler:
         self._serial_number = 1
         self.interface_addresses = netifaces.ifaddresses(interface)
 
-    def _get_mac(self) -> None:
-        try:
-            mac_address = self._wait_for_network_interface_address(netifaces.AF_LINK)
-            return mac_address
-        except (KeyError, IndexError) as e:
-            logger.error(f'No MAC address found, restart the system')
-            raise
-    
-    def _get_ip(self) -> None: 
-        try:
-            ip_address = self._wait_for_network_interface_address(netifaces.AF_INET)
-            return ip_address
-        except (KeyError, IndexError) as e:
-            logger.error(f'No IP address found, restart the system')
-            raise
-
     def _wait_for_network_interface_address(self, address_family: int) -> str:
         while True:
             try:
@@ -33,17 +17,33 @@ class NetworkInterfaceHandler:
             except (KeyError, IndexError):
                 logger.debug('Network interface not available yet. Retrying...')
                 time.sleep(1)
+
+    def _get_mac(self) -> str | None:
+        try:
+            mac_address = self._wait_for_network_interface_address(netifaces.AF_LINK)
+            return mac_address
+        except (KeyError, IndexError) as e:
+            logger.error(f'No MAC address found, restart the system')
+            raise
     
-    def _get_uptime(self) -> None:
+    def _get_ip(self) -> str | None: 
+        try:
+            ip_address = self._wait_for_network_interface_address(netifaces.AF_INET)
+            return ip_address
+        except (KeyError, IndexError) as e:
+            logger.error(f'No IP address found, restart the system')
+            raise
+    
+    def _get_uptime(self) -> int | float | None:
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = float(f.readline().split()[0])
             return uptime_seconds
         
-    def _get_fiber_id(self) -> None:
+    def _get_fiber_id(self) -> int:
         fiber_id = self._serial_number
         return fiber_id
     
-    def _reboot(self, body):
+    def _reboot(self, body: dict) -> None:
         if body != None:
             time.sleep(body['delay'])
 
