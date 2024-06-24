a9c0f8925749619124b89605933c0447e28f0aa6
nikitach507 <nikita.chuprin@hardwario.com>
2024-06-24 12:30:58 +0200

    Update installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 pip uninstall -y fiber
-
 cd /app
 
+mkdir -p /etc/fiber /var/fiber
+
 cp /app/install/roles/system/files/rc.local /etc/rc.local
-
 cp /app/install/roles/system/files/journald.conf /etc/systemd/journald.conf
-
 cp /app/install/roles/fiber-packages/files/fiber-core.service /etc/systemd/system/fiber-core.service
+cp /app/fiber/display/LiberationSans-BaDn.ttf /etc/fiber
 
-python /app/fiber/setup.py install
+pip install .
 
-mkdir /etc/fiber -p
-
-mkdir /var/fiber -p
-
-cp /app/config/config.yaml /etc/fiber/config.yaml
+cp /app/config/config.yaml /data/config.yaml
 
 systemctl enable fiber-core.service
-
-systemctl start fiber-core.service
-
 systemctl restart fiber-core.service
 
-# tmpfs /var/log  tmpfs defaults,noatime,nosuid,mode=0755,size=512m 0 0
+rm -rf /app
 
-# echo "Running Ansible playbook to install..."
-
-# ansible-playbook -i /app/install/hosts /app/install/install.yml --ask-pass -vvv
-
-# echo "Installation complete."
-
+mkdir -p /app
